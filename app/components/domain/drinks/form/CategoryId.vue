c11ff78881c68001dd737334e208a17cfca0b805
RaulSAraujo <raul.araujo.2000@hotmail.com>
2025-10-13 17:27:44 -0300

    ♻️ refactor: category id

--- app/components/domain/drinks/form/CategoryId.vue
+++ app/components/domain/drinks/form/CategoryId.vue
@@ -2,27 +2,15 @@
 import type { DrinkCategories } from "~/types/drink-categories";
 
 defineProps<{ categories: DrinkCategories[] }>();
-
-const search = ref<string>("");
 </script>
 
 <template>
   <UiComboboxField
-    v-model:search="search"
     label="Categoria"
     :items="categories"
     item-value="id"
     item-title="name"
     hide-details="auto"
     prepend-inner-icon="mdi-information-variant"
-  >
-    <template #no-data>
-      <v-list-item>
-        <v-list-item-title>
-          No results matching "<strong>{{ search }}</strong
-          >". Press <kbd>enter</kbd> to create a new one
-        </v-list-item-title>
-      </v-list-item>
-    </template>
-  </UiComboboxField>
+  />
 </template>
